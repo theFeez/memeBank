8cae19538df05290f23b53e5bba02de9b98b6f75
theFeez <zachfeez@knights.ucf.edu>
2016-11-30 22:54:46 -0500

    fixed admin.js image display

--- views/admin.js
+++ views/admin.js
@@ -10,6 +10,7 @@ $.get('/loadPics',function(data){
     var pics='';
     console.log(data);
     for(var i in data.pics){
+        console.log(i);
         console.log(data.pics[i].id);
         pics = pics+'<li><input type=\'checkbox\' name=ids[] value='+data.pics[i].id+'><img src=\''+data.pics[i].url+'\'</img></li>';
 
