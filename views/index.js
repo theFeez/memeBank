86cb46e3d3e978c0ceb8b427ed7f822803f03125
Fulton Garcia <fultongarcia@gmail.com>
2017-03-22 18:40:02 -0400

    more grid fixes

--- views/index.js
+++ views/index.js
@@ -16,7 +16,7 @@ function refreshPics(){
               pics = pics + '<div class="row">';
             }
 
-            pics = pics + '<a href="'+ data[i].url + '" target="_blank"><div class="col-md-2 col-xs-6 col-sm-4" style="background-image: url('+ data[i].url +');background-repeat: no-repeat; background-size: contain; height: 200px; padding-bottom: 20px;"></div></a>';
+            pics = pics + '<a href="'+ data[i].url + '" target="_blank"><div class="col-md-2 col-xs-6 col-sm-4 img-thumbnail" style="background-image: url('+ data[i].url +');background-repeat: no-repeat; background-position: center; background-size: cover; height: 200px; padding-bottom: 20px;"></div></a>';
 
             if(counter == 5){
               pics = pics + '</div>';
