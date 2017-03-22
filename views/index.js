ad767109ed74aa0ff69d1314d1d71ac3f884c4e5
Fulton Garcia <fultongarcia@gmail.com>
2017-03-22 17:29:10 -0400

    adding responsiveness

--- views/index.js
+++ views/index.js
@@ -7,25 +7,24 @@ var login;
 
 
 function refreshPics(){
-    console.log('ran it');
     $.get('/loadPics',function(data){
 
         console.log(data);
         var counter = 0;
         for(var i in data){
-            console.log(data[i].url);
-            console.log(counter);
             if(counter == 0){
               pics = pics + '<div class="row">';
             }
 
-            pics = pics + '<div class="col-md-4 col-xs-4 col-sm-4"><img src=\''+data[i].url+'\'class="img-responsive img-grid"> </img></div>';
+            pics = pics + '<div class="col-md-4 col-xs-12 col-sm-6"><img src=\''+data[i].url+'\'class="img-responsive img-grid"> </img></div>';
 
             if(counter == 2){
               pics = pics + '</div>';
               counter = 0;
             }
-            counter++;
+            else{
+              counter++;
+            }
         }
         if(counter > 2){
           pics = pics + '</div>';
