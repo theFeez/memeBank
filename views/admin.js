947b2209b78cb23b23960e06c23bac57e588b4e7
Fulton Garcia <fultongarcia@gmail.com>
2017-03-22 21:51:40 -0400

    moved admin login

--- views/admin.js
+++ views/admin.js
@@ -1,22 +1,32 @@
 var loggedIn;
 var nuke = function(){
     $.get('/nuke',function(data){
-        console.log(data);  
+        console.log(data);
     })
 }
 
 
 $.get('/loadPics',function(data){
-    var pics='';
-    console.log(data);
-    for(var i in data){
-        console.log(i);
-        console.log(data[i].id);
-        pics = pics+'<li><input type=\'checkbox\' name=ids[] value='+data[i].id+'><img src=\''+data[i].url+'\'</img></li>';
+  var counter = 0;
+  for(var i in data){
+      if(counter == 0){
+        pics = pics + '<div class="row">';
+      }
 
-    }
+      pics = pics + '<a href="'+ data[i].url + '" target="_blank"><div class="col-md-2 col-xs-6 col-sm-4 img-thumbnail" style="background-image: url('+ data[i].url +');background-repeat: no-repeat; background-position: center; background-size: cover; height: 200px;"></div></a>';
 
-    $('#picList').html(pics);
+      if(counter == 5){
+        pics = pics + '</div>';
+        counter = 0;
+      }
+      else{
+        counter++;
+      }
+  }
+  if(counter > 5){
+    pics = pics + '</div>';
+  }
+  $('#picList').html(pics);
 
 
-});  
+});
